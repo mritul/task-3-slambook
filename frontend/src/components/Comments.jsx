3ebbae05b68f67c03c6caf806d2c435a1e635f4b
Mritul <mritul2003@gmail.com>
2022-08-01 20:46:01 +0530

    Removed localhost origin from cors

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Comment from "./Comment";
 import { useAuth } from "../utils/AuthContext";
@@ -8,18 +7,7 @@ const Comments = () => {
   const [comments, setComments] = useState([]);
   const auth = useAuth();
   useEffect(() => {
-    // // The below fetch is to get all the comments on the user from the backend and display them
-    // axios({
-    //   method: "GET",
-    //   withCredentials: true,
-    //   url: "https://slambook-back-end.herokuapp.com/authenticate",
-    // })
-    //   .then((res) => {
-    //     setComments(res.data.user.slambookAnswers);
-    //   })
-    //   .catch((err) => {
-    //     throw err;
-    //   });
+    // To display the comments(slambook answers) made on the user by other users
     setComments(auth.user.slambookAnswers);
   }, [auth.user]);
   return (
