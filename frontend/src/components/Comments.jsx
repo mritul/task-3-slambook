ff6c6693e3b004ed4e7a639d14e6e44f28ed8fde
Mritul <mritul2003@gmail.com>
2022-08-01 20:43:36 +0530

    reverted origin

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -8,19 +8,20 @@ const Comments = () => {
   const [comments, setComments] = useState([]);
   const auth = useAuth();
   useEffect(() => {
-    // The below fetch is to get all the comments on the user from the backend and display them
-    axios({
-      method: "GET",
-      withCredentials: true,
-      url: "https://slambook-back-end.herokuapp.com/authenticate",
-    })
-      .then((res) => {
-        setComments(res.data.user.slambookAnswers);
-      })
-      .catch((err) => {
-        throw err;
-      });
-  }, []);
+    // // The below fetch is to get all the comments on the user from the backend and display them
+    // axios({
+    //   method: "GET",
+    //   withCredentials: true,
+    //   url: "https://slambook-back-end.herokuapp.com/authenticate",
+    // })
+    //   .then((res) => {
+    //     setComments(res.data.user.slambookAnswers);
+    //   })
+    //   .catch((err) => {
+    //     throw err;
+    //   });
+    setComments(auth.user.slambookAnswers);
+  }, [auth.user]);
   return (
     <div className="Comments">
       {comments ? (
